f2820194232b9effddf14539d10c0637d83d5bec
Onur Cinar <onur.cinar@gmail.com>
2022-06-24 14:39:01 -0700

    Bollinger Bands standard deviation calculation is different from other implementations (#106)
    Fixes #105

--- src/indicator/volatility/mstd.test.ts
+++ src/indicator/volatility/mstd.test.ts
@@ -8,8 +8,8 @@ import { mstd } from './mstd';
 describe('Standard deviation', () => {
   it('should be able to compute std', () => {
     const values = [2, 4, 6, 8, 12, 14, 16, 18, 20];
-    const expected = [0, 0, 0, 2.236, 2.958, 3.162, 2.958, 2.236, 2.236];
-    const period = 4;
+    const expected = [0, 1, 1, 1, 2, 1, 1, 1, 1];
+    const period = 2;
 
     const actual = mstd(period, values);
     deepStrictEqual(roundDigitsAll(3, actual), expected);
