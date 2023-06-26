ded8194135b4764c01afc286955567c7e54add20
Abhishek Mittal <abhishekmittal964@gmail.com>
2023-06-26 09:33:08 +0530

    Fixed a logical error in ROC Indicator implementation

--- src/indicator/momentum/priceRateOfChange.test.ts
+++ src/indicator/momentum/priceRateOfChange.test.ts
@@ -4,8 +4,8 @@ import { roc } from "./priceRateOfChange";
 
 describe("Price Rate of Change (ROC)", () => {
   it("should be able to compute ROC", () => {
-    const values = [1, 3, 5, 4, 2, 4];
-    const expected = [0, 0, 400, 33.33, -60, 0];
+    const values = [1, 3, 5, 4, 2, 5, 3];
+    const expected = [0, 0, 0, 300, -33.33, 0, -25];
     const period = 3;
 
     const actual = roc(period, values);
--- src/indicator/momentum/priceRateOfChange.ts
+++ src/indicator/momentum/priceRateOfChange.ts
@@ -8,10 +8,12 @@ export function roc(period: number, values: number[]): number[] {
   const result = new Array<number>(values.length);
 
   for (let i = 0; i < values.length; i++) {
-    if (i < (period - 1)) {
+    if (i < period) {
       // Setting indicator values to 0 for days before the 1st period.
       result[i] = 0;
-    } else result[i] = (values[i] / values[i - period + 1] - 1) * 100;
+    } else {
+      result[i] = (values[i] / values[i - period] - 1) * 100;
+    }
   }
 
   return result;
