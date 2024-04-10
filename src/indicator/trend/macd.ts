699db39209f8a9aae4f991cbbffa649dcaa76df4
Patrick Cason <me@patrickcason.com>
2024-04-09 20:58:30 -0500

    Finished macd

--- src/indicator/trend/macd.ts
+++ src/indicator/trend/macd.ts
@@ -10,20 +10,43 @@ export interface MacdResult {
 }
 
 /**
+ * Optional configuration of Macd parameters.
+ */
+export interface MacdConfig {
+  fast?: number;
+  slow?: number;
+  signal?: number;
+}
+
+/**
+ * The default configuration of Macd.
+ */
+export const MacdDefaultConfig: Required<MacdConfig> = {
+  fast: 12,
+  slow: 26,
+  signal: 9,
+};
+
+/**
  * Moving Average Convergence Divergence (MACD).
  *
  * MACD = 12-Period EMA - 26-Period EMA.
  * Signal = 9-Period EMA of MACD.
  *
  * @param closings closing values.
+ * @param config configuration.
  * @return macd result.
  */
-export function macd(closings: number[]): MacdResult {
-  const ema12 = ema(closings, { period: 12 });
-  const ema26 = ema(closings, { period: 26 });
+export function macd(closings: number[], config: MacdConfig = {}): MacdResult {
+  const { fast, slow, signal } = {
+    ...MacdDefaultConfig,
+    ...config,
+  };
+  const emaFast = ema(closings, { period: fast });
+  const emaSlow = ema(closings, { period: slow });
 
-  const macdLine = subtract(ema12, ema26);
-  const signalLine = ema(macdLine, { period: 9 });
+  const macdLine = subtract(emaFast, emaSlow);
+  const signalLine = ema(macdLine, { period: signal });
 
   return {
     macdLine,
